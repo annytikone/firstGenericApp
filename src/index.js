d07de243a67a16a6f8b476dc769dd04d72777785
Akash Pande <apande@techracers.io>
2021-06-08 00:03:28 +0530

    dsbgjhc j

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+//var express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log(' app on 3000!,changed node --v to 9 from 12');
+});
+
+const sayHello = 'airbnb rule';
+console.log(sayHello);
